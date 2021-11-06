fe7940fb4d64150f2c7a6c88985d32ea3502d127
Marvin Ding <jaegerding@gmail.com>
2021-11-06 17:17:40 -0400

    coding part completed

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,7 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
+  //"URI": "mongodb://localhost/books229"
+
+  //cloud MongoDb deployment ->
+  "URI": "mongodb+srv://marvinding:JeVeKV5Onk8BHYRI@cluster0.kbwqp.mongodb.net/book_list?retryWrites=true&w=majority"
 };
--- server/routes/books.js
+++ server/routes/books.js
@@ -24,29 +24,66 @@ router.get('/', (req, res, next) => {
 });
 
 //  GET the Book Details page in order to add a new Book
-router.get('/add', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+router.get('/details', (req, res, next) => {
+     book.find((err, books) => {
+      if (err) {
+        return console.error(err);
+      }
+      else {
+        res.render('books/details', {
+          title: 'Add New Book',
+          books: books
+        });
+      }
+    });
 });
 
 // POST process the Book Details page and create a new Book - CREATE
-router.post('/add', (req, res, next) => {
+router.post('/details', (req, res, next) => {
+  let newBook = book({
+    "Title": req.body.title,
+    "Author": req.body.author,
+    "Genre": req.body.genre,
+    "Price": req.body.price
+});
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+book.create(newBook, (err, Book) =>{
+    if(err)
+    {
+        console.log(err);
+        res.end(err);
+    }
+    else
+    {
+        // refresh the book list
+        res.redirect('/books');
+    }
+});
 });
 
 // GET the Book Details page in order to edit an existing Book
 router.get('/:id', (req, res, next) => {
-
     /*****************
      * ADD CODE HERE *
      *****************/
+     let id = req.params.id;
+
+     book.findById(id, (err, bookToEdit) => {
+         if(err)
+         {
+             console.log(err);
+             res.end(err);
+         }
+         else
+         {
+             //show the edit view
+             res.render('books/details', 
+             {title: 'Edit Book',
+             books: bookToEdit
+            })
+         }
+     });
+
 });
 
 // POST - process the information passed from the details form and update the document
@@ -55,7 +92,27 @@ router.post('/:id', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
+    let id = req.params.id
+    let updateBook = book({
+      "_id": id,
+      "Title": req.body.title,
+      "Author": req.body.author,
+      "Genre": req.body.genre,
+      "Price": req.body.price
+    })
 
+    book.updateOne({_id: id}, updateBook, (err) => {
+      if(err)
+      {
+          console.log(err);
+          res.end(err);
+      }
+      else
+      {
+          // refresh the book list
+          res.redirect('/books');
+      }
+  });
 });
 
 // GET - process the delete by user id
@@ -64,6 +121,20 @@ router.get('/delete/:id', (req, res, next) => {
     /*****************
      * ADD CODE HERE *
      *****************/
+     let id = req.params.id;
+
+     book.remove({_id: id}, (err) => {
+         if(err)
+         {
+             console.log(err);
+             res.end(err);
+         }
+         else
+         {
+              // refresh the book list
+              res.redirect('/books');
+         }
+     });
 });
 
 
